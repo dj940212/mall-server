6288167fd5741e67bddb3d111939481bcad55d59
dj940212 <2902273280@qq.com>
2017-07-11 14:59:28 +0800

    完成购物车数据获取接口

--- routes/users.js
+++ routes/users.js
@@ -34,6 +34,12 @@ router.post("/login",function(req,res,next){
                 userName:userDoc.userName
             }
           })
+       }else{
+        res.json({
+            status:'1',
+            msg:'未登录',
+            result:''
+        });
        }
     }
   })
@@ -52,6 +58,27 @@ router.post("/logout",function(req,res,next){
         result:''
     })
 })
-
+// 查询当前用户的购物车数据
+router.get("/cartList",function(req,res,next){
+    var userId = req.cookies.userId;
+    User.findOne({userId:userId},function(err,doc){
+        if (err) {
+           res.json({
+            status:'1',
+            msg:err.message,
+            result:''
+           });
+        }else{
+            if (doc) {
+                console.log('获取购物车数据成功')
+                res.json({
+                  status:'1',
+                  msg:'',
+                  result:doc.cartList
+                });
+            }
+        }
+    })
+})
 
 module.exports = router;
